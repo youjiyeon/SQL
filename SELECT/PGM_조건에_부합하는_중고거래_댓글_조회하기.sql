2da15b368bb9392754d77e6d8804f66c7d3397cc
youjiyeon <57094856+youjiyeon@users.noreply.github.com>
2023-05-22 21:44:40 +0900

    Add files via upload

---
+++ SELECT/PGM_조건에_부합하는_중고거래_댓글_조회하기.sql
@@ -0,0 +1,7 @@
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, 
+DATE_FORMAT(r.CREATED_DATE,"%Y-%m-%d")
+from USED_GOODS_BOARD as b
+join USED_GOODS_REPLY as r
+    on b.BOARD_ID = r.BOARD_ID
+where b.CREATED_DATE like "2022-10%"
+order by r.CREATED_DATE asc, b.TITLE asc;
